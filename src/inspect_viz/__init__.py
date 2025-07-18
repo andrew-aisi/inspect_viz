e4f5154d05886adf45c0f708f11b4220223ec0aa
J.J. Allaire <jj.allaire@gmail.com>
2025-07-18 07:04:45 -0400

    dynamic versioning for hatchling

--- src/inspect_viz/__init__.py
+++ src/inspect_viz/__init__.py
@@ -1,5 +1,11 @@
 from ._core import Component, Data, Param, ParamValue, Selection
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
+
 __all__ = [
     "Data",
     "Param",
