c61568f204f6e2fb9202da4ee1cb7194634e36f2
J.J. Allaire <jj.allaire@gmail.com>
2025-08-02 17:43:19 -0400

    guide

--- docs/scripts/post-render.sh
+++ docs/scripts/post-render.sh
@@ -4,6 +4,8 @@ files=("index" "components-plots" "components-marks" "components-links" "compone
 
 
 if [ "$QUARTO_PROJECT_RENDER_ALL" = "1" ]; then
+    llms_guide="_site/llms-guide.txt"
+    rm -f "${llms_guide}"
     llms_full="_site/llms-full.txt"
     rm -f "${llms_full}"
     mv _quarto.yml _quarto.yml.bak
@@ -13,6 +15,10 @@ if [ "$QUARTO_PROJECT_RENDER_ALL" = "1" ]; then
         output_file="${file}.md"
         cat "${output_file}" >> "${llms_full}"
         echo "" >> "${llms_full}"
+        if [[ ! "${file}" == reference/* ]]; then
+            cat "${output_file}" >> "${llms_guide}"
+            echo "" >> "${llms_guide}"
+        fi
         mv $output_file "_site/${file}.html.md"
     done
     mv _quarto.yml.bak _quarto.yml
