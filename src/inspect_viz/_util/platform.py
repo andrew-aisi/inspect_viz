b94466290aba786b82bf2f89c86a5ac15806f1d9
J.J. Allaire <jj.allaire@gmail.com>
2025-08-05 08:15:00 -0400

    quarto: format detection for v1.8

--- src/inspect_viz/_util/platform.py
+++ src/inspect_viz/_util/platform.py
@@ -1,4 +1,10 @@
+import json
 import os
+from typing import cast
+
+from typing_extensions import TypedDict
+
+from inspect_viz._util.notgiven import NOT_GIVEN, NotGiven
 
 
 def running_in_quarto() -> bool:
@@ -7,22 +13,46 @@ def running_in_quarto() -> bool:
 
 def quarto_png() -> bool:
     if running_in_quarto():
-        format = os.environ.get("QUARTO_OUTPUT_BASE_FORMAT", "")
-        if format:
-            return format not in ["html", "html4", "html5", "dashboard"]
+        execute_info = quarto_execute_info()
+        if execute_info is not None:
+            return execute_info["format"]["identifier"]["base-format"] not in [
+                "html",
+                "html4",
+                "html5",
+                "dashboard",
+            ]
         else:
             return os.environ.get("QUARTO_FIG_FORMAT", "") in ["pdf", "svg"]
     else:
         return False
 
 
-def running_in_colab() -> bool:
-    try:
-        import google.colab  # type: ignore # noqa: F401
+QuartoFormatIdentifier = TypedDict(
+    "QuartoFormatIdentifier",
+    {"display-name": str, "target-format": str, "base-format": str},
+)
 
-        return True
-    except ImportError:
-        return False
+QuartoFormat = TypedDict("QuartoFormat", {"identifier": QuartoFormatIdentifier})
+
+
+QuartoExecuteInfo = TypedDict(
+    "QuartoExecuteInfo", {"document-path": str, "format": QuartoFormat}
+)
+
+_quarto_execute_info: QuartoExecuteInfo | None | NotGiven = NOT_GIVEN
+
+
+def quarto_execute_info() -> QuartoExecuteInfo | None:
+    global _quarto_execute_info
+    if isinstance(_quarto_execute_info, NotGiven):
+        execute_info_file = os.environ.get("QUARTO_EXECUTE_INFO", "")
+        if execute_info_file:
+            _quarto_execute_info = cast(
+                QuartoExecuteInfo, json.load(open(execute_info_file))
+            )
+        else:
+            _quarto_execute_info = None
+    return _quarto_execute_info
 
 
 def quarto_fig_size() -> tuple[int, int] | None:
@@ -33,3 +63,12 @@ def quarto_fig_size() -> tuple[int, int] | None:
             return (int(float(fig_width) * 96), int(float(fig_height) * 96))
 
     return None
+
+
+def running_in_colab() -> bool:
+    try:
+        import google.colab  # type: ignore # noqa: F401
+
+        return True
+    except ImportError:
+        return False
