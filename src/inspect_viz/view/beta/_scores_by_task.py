ca7dc1c524c4cf90e2c36862934afc60e3e278aa
Charles Teague <charles@merdianlabs.ai>
2025-08-06 14:53:10 -0400

    clean up scores by task

--- src/inspect_viz/view/beta/_scores_by_task.py
+++ src/inspect_viz/view/beta/_scores_by_task.py
@@ -17,12 +17,12 @@ from inspect_viz.transform import sql
 
 def scores_by_task(
     data: Data,
-    x: str = "model_display_name",
-    fx: str = "task_display_name",
-    y: str = "score_headline_value",
-    y_stderr: str = "score_headline_stderr",
-    y_ci: bool | float = 0.95,
-    y_label: str | None | NotGiven = NOT_GIVEN,
+    model_name: str = "model_display_name",
+    task_name: str = "task_display_name",
+    score_value: str = "score_headline_value",
+    score_stderr: str = "score_headline_stderr",
+    score_label: str | None | NotGiven = NOT_GIVEN,
+    ci: bool | float = 0.95,
     title: str | Title | None = None,
     marks: Marks | None = None,
     width: float | Param | None = None,
@@ -35,12 +35,12 @@ def scores_by_task(
 
     Args:
        data: Evals data table. This is typically created using a data frame read with the inspect `evals_df()` function.
-       x: Name of field for x axis (defaults to "model_display_name")
-       fx: Name of field for x facet (defaults to "task_display_name")
-       y: Name of field for y axis (defaults to "score_headline_value").
-       y_stderr: Name of field for stderr (defaults to "score_headline_metric").
-       y_ci: Confidence interval (e.g. 0.80, 0.90, 0.95, etc.). Defaults to 0.95.
-       y_label: Y axis label (pass None for no label).
+       model_name: Name of field for the model name (defaults to "model_display_name")
+       task_name: Name of field for the task name (defaults to "task_display_name")
+       score_value: Name of field for the score value (defaults to "score_headline_value").
+       score_stderr: Name of field for stderr (defaults to "score_headline_metric").
+       score_label: Score axis label (pass None for no label).
+       ci: Confidence interval (e.g. 0.80, 0.90, 0.95, etc.). Defaults to 0.95.
        title: Title for plot (`str` or mark created with the `title()` function).
        marks: Additional marks to include in the plot.
        width: The outer width of the plot in pixels, including margins. Defaults to 700.
@@ -48,14 +48,14 @@ def scores_by_task(
        **attributes: Additional `PlotAttributes`. By default, the `margin_bottom` are is set to 10 pixels and `x_ticks` is set to `[]`.
     """
     # resolve the x
-    if x == "model_display_name" and "model_display_name" not in data.columns:
+    if model_name == "model_display_name" and "model_display_name" not in data.columns:
         # fallback to using the raw model string
-        x = "model"
+        model_name = "model"
 
     # resolve the fx
-    if fx == "task_display_name" and "task_display_name" not in data.columns:
+    if task_name == "task_display_name" and "task_display_name" not in data.columns:
         # fallback to using the raw task name string
-        fx = "task_name"
+        task_name = "task_name"
 
     # resolve the title
     if isinstance(title, str):
@@ -66,28 +66,38 @@ def scores_by_task(
 
     # establish channels
     channels: dict[str, str] = {}
-    if fx == "task_name" or fx == "task_display_name":
-        channels["Task"] = fx
-    if x == "model" or x == "model_display_name":
-        channels["Model"] = x
-    if y == "score_headline_value":
-        channels["Score"] = y
+    if task_name == "task_name" or task_name == "task_display_name":
+        channels["Task"] = task_name
+    if model_name == "model" or model_name == "model_display_name":
+        channels["Model"] = model_name
+    if score_value == "score_headline_value":
+        channels["Score"] = score_value
     resolve_log_viewer_channel(data, channels)
 
     # start with bar plot
-    components = [bar_y(data, x=x, fx=fx, y=y, fill=x, channels=channels, tip=True)]
+    components = [
+        bar_y(
+            data,
+            x=model_name,
+            fx=task_name,
+            y=score_value,
+            fill=model_name,
+            channels=channels,
+            tip=True,
+        )
+    ]
 
     # add ci if requested
-    if y_ci is not False:
-        y_ci = 0.95 if y_ci is True else y_ci
-        z_alpha = z_score(y_ci)
+    if ci is not False:
+        ci = 0.95 if ci is True else ci
+        z_alpha = z_score(ci)
         components.append(
             rule_x(
                 data,
-                x=x,
-                fx=fx,
-                y1=sql(f"{y} - ({z_alpha} * {y_stderr})"),
-                y2=sql(f"{y} + ({z_alpha} * {y_stderr})"),
+                x=model_name,
+                fx=task_name,
+                y1=sql(f"{score_value} - ({z_alpha} * {score_stderr})"),
+                y2=sql(f"{score_value} + ({z_alpha} * {score_stderr})"),
                 stroke="black",
                 marker="tick-x",
             ),
@@ -109,7 +119,7 @@ def scores_by_task(
         legend=legend("color", frame_anchor="bottom"),
         x_label=None,
         fx_label=None,
-        y_label=y_label,
+        y_label=score_label,
         title=title,
         width=width,
         height=height,
